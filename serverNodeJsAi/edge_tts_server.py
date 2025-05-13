ce267d41c650f365d9819093b27156c4c82c369d
Ho Thai <thai.hx04@gmail.com>
2025-05-13 08:45:24 +0700

    Docs

--- serverNodeJsAi/edge_tts_server.py
+++ serverNodeJsAi/edge_tts_server.py
@@ -27,8 +27,8 @@ def text_to_speech():
         audio_data = loop.run_until_complete(generate_audio_data(text, voice, rate, volume))
 
         #lưu file mp3 vào ./sound
-        with open('./sound_debug/sound_tts_received.mp3', 'wb') as f:
-            f.write(audio_data)
+        # with open('./sound_debug/sound_tts_received.mp3', 'wb') as f:
+        #     f.write(audio_data)
         
         # Chuyển đổi MP3 sang WAV sử dụng pydub
         mp3_audio = AudioSegment.from_mp3(io.BytesIO(audio_data))
